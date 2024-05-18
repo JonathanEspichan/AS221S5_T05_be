2a5cb58b8ec3bdfb18090ef20712effbb61aacde
jonathan <jonathan.espichan@vallegrande.edu.pe>
2024-05-17 22:18:05 -0500

    traslator/text/AI

---
+++ src/test/java/pe/edu/vallegrande/appdemo/AppdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package pe.edu.vallegrande.appdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+import pe.edu.vallegrande.cassiatec.TraductorIA_Application;
+
+@ContextConfiguration(classes = TraductorIA_Application.class)
+class AppdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
